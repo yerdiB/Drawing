b10f23eb587116099efb235999b924bdb57c96be
yerdi <yerdi.isman@gmail.com>
2023-01-13 23:51:03 +0600

    first commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Yerdaulet Ismanaliyev on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
